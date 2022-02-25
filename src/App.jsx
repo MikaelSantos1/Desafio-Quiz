b2b29fb3bb4d28c968b11b3b0a9065238e2ee79c
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-02-24 22:44:53 -0300

    errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { GlobalStyle } from "./styles/globalStyle";
 import { Header } from './components/Header'
 import { ContextProvider } from "./Hooks/UserContext";
 import { UserInput } from "./components/UserInput";
-import { Quiz } from "./components/Quiz";
-import { useUser } from "./Hooks/UserContext";
+
 
 function App() {
   return (
